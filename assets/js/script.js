c246105e86562e7a14933874d54e3a6852316820
unknown <beiranvand.somaye@gmail.com>
2022-11-09 23:10:57 +0330

    add focus and blur event to radio buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  $(".thank-card").hide();
+  $(".thank-card__btn").on("click", function () {
+    $(".card").show();
+    $(".thank-card").hide();
+  });
+  $(".card__submit").on("click", function (e) {
+    e.preventDefault();
+    let r = $("input[name=score]:checked").val();
+    $(".card").hide();
+    $(".thank-card").show();
+    $(".thank-card__result").text("You selected " + r + " out of 5");
+  });
+  $("input[name='score']").on("focus", function () {
+    $(this).parent().css("background-color", "orange");
+  });
+  $("input[name='score']").on("blur", function () {
+    $(this).parent().css("background-color", "white");
+  });
+});
