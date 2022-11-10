317768ed2e5464ad620884803f30c6652717413e
unknown <beiranvand.somaye@gmail.com>
2022-11-10 13:40:51 +0330

    first acceptable vesion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  $(".thank-card").hide();
-  $(".thank-card__btn").on("click", function () {
-    $(".card").show();
-    $(".thank-card").hide();
-  });
   $(".card__submit").on("click", function (e) {
     e.preventDefault();
     let r = $("input[name=score]:checked").val();
     $(".card").hide();
-    $(".thank-card").show();
+    $(".thank-card").css("display", "flex");
     $(".thank-card__result").text("You selected " + r + " out of 5");
   });
+  $(".thank-card__btn").on("click", function () {
+    $(".card").show();
+    $(".thank-card").hide();
+  });
   $("input[name='score']").on("focus", function () {
-    $(this).parent().css("background-color", "orange");
+    $(this).parent().addClass("selected");
   });
   $("input[name='score']").on("blur", function () {
-    $(this).parent().css("background-color", "white");
+    $(this).parent().removeClass("selected");
   });
 });
